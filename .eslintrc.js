8fa7f63da555612e65f15d602f6b2c5bbf47ba80
TianxiangR <ren644120622@gmail.com>
2024-04-12 21:57:14 -0600

    refactor: initialize the project created by create-next-app

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    'env': {
+      'browser': true,
+      'es2021': true
+    },
+    'extends': [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+    ],
+    'overrides': [
+      {
+        'env': {
+          'node': true
+        },
+        'files': [
+          '.eslintrc.{js,cjs}'
+        ],
+        'parserOptions': {
+          'sourceType': 'script'
+        }
+      }
+    ],
+    ignorePatterns: ['/*.js', '/*.ts', 'build/**', 'dist/**', 'node_modules/**'],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+      'ecmaVersion': 'latest',
+      'sourceType': 'module'
+    },
+    'plugins': [
+      '@typescript-eslint',
+      'react',
+      'simple-import-sort',
+      'import'
+    ],
+    'rules': {
+      'indent': [
+        'error',
+        2
+      ],
+      'quotes': [
+        'error',
+        'single'
+      ],
+      'semi': [
+        'error',
+        'always'
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/no-duplicates': 'error',
+      'import/newline-after-import': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    }
+  };
