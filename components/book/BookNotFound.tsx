705b53503b4edc983ab43c6afe79f6fea17e9c79
TianxiangR <ren644120622@gmail.com>
2024-04-14 22:37:26 -0600

    feat: created the BookNotFound Page

---
+++ components/book/BookNotFound.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import { MdError } from 'react-icons/md';
+
+import { createNavigateBack } from '@/lib/utils';
+
+import { Button } from '../ui/button';
+import { Card } from '../ui/card';
+
+function BookNotFound() {
+  const router = useRouter();
+  const navigateBack = createNavigateBack(router);
+
+  return (
+    <main className="w-full h-screen flex">
+      <Card className='p-10 max-w-[800px] ml-auto mr-auto mt-auto mb-auto w-full flex flex-col justify-center items-center'>
+        <div className="flex justify-center items-center">
+          <MdError className="size-7 text-red-500 mr-2" />
+          <p className="text-center">Book not found</p>
+        </div>
+
+        <Button className='w-fit' variant='link'>
+          <span onClick={navigateBack}>Go back</span>
+        </Button>
+      </Card>
+    </main>
+  );
+}
+
+export default BookNotFound;
