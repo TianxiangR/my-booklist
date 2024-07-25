7811e959e60468a672cdb4800e6f17700d64ded4
TianxiangR <ren644120622@gmail.com>
2024-07-24 20:39:59 -0400

    fix: replace fetch with https + cache

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,7 @@
 'use server';
-import { revalidateTag } from 'next/cache';
+import https from 'https';
+import { revalidateTag , unstable_cache as cache, unstable_noStore as noStore } from 'next/cache';
+import { notFound } from 'next/navigation';
 
 import { Book, BookApi } from '@/app/types';
 
@@ -15,90 +17,169 @@ export const revalidateBookById = async (id: string) => {
   revalidateTag(`getBookById-${id}`);
 };
 
-export const getBooks = async (): Promise<Book[]> => {
-  const response = await fetch(`${base_url}/books`, {
-    cache: 'no-store',
-    next: {
-      tags: [getBooksTag],
+export const getBooks = cache(async (): Promise<Book[]> => {
+  return new Promise((resolve, reject) => {
+    try {
+      https.get(`${base_url}/books`, (res) => {
+        if (res.statusCode !== 200) {
+          reject(new Error('Api error'));
+        }
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          const books: BookApi[] = JSON.parse(data);
+          resolve(books.map((book) => ({
+            ...book,
+            price: Number(book.price),
+          })));
+        });
+      });
+    } catch (error) {
+      reject(error);
     }
   });
-  const data: BookApi[] = await response.json();
-  return data.map((book) => ({
-    ...book,
-    price: Number(book.price),
-  }));
-};
+}, [getBooksTag], {
+  tags: [getBooksTag],
+});
 
 export const createBook = async (book: Omit<Book, 'id'>): Promise<Book> => {
   const bookApi: Omit<BookApi, 'id'> = {
     ...book,
     price: String(book.price),
   };
-  const response = await fetch(`${base_url}/books`, {
-    method: 'POST',
-    body: JSON.stringify(bookApi),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  const data: BookApi = await response.json();
-  await revalidateBooks();
-  return {
-    ...data,
-    price: Number(data.price),
-  };
-};
-
-export const getBookById = async (id: string): Promise<Book | null> => {
-  const response = await fetch(`${base_url}/books/${id}`, {
-    cache: 'no-store',
-    next: {
-      tags: [`getBookById-${id}`],
+  return new Promise((resolve, reject) => {
+    try {
+      const req = https.request(
+        `${base_url}/books`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+        (res) => {
+          if (res.statusCode !== 201) {
+            reject(new Error('Api error'));
+          }
+          let data = '';
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            const book: BookApi = JSON.parse(data);
+            revalidateBooks();
+            resolve({
+              ...book,
+              price: Number(book.price),
+            });
+          });
+        },
+      );
+      req.write(JSON.stringify(bookApi));
+      req.end();
+    } catch (error) {
+      reject(error);
     }
   });
-
-  if (response.status === 404) {
-    return null;
-  }
-
-  if (!response.ok) {
-    throw new Error('Api error');
-  }
-
-  const data: BookApi = await response.json();
-  return {
-    ...data,
-    price: Number(data.price),
-  };
 };
+
+export const getBookById = async (id: string) => cache(async (id: string): Promise<Book | null> => {
+  return new Promise((resolve, reject) => {
+    try {
+      https.get(`${base_url}/books/${id}`, (res) => {
+        if (res.statusCode === 404) {
+          resolve(null);
+        }
+        if (res.statusCode !== 200) {
+          reject(new Error('Api error'));
+        }
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          const book: BookApi = JSON.parse(data);
+          resolve({
+            ...book,
+            price: Number(book.price),
+          });
+        });
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}, [`getBookById-${id}`], { 
+  tags: [`getBookById-${id}`],
+  revalidate: 1,
+})(id);
 
 export const updateBook = async (book: Book): Promise<Book> => {
   const bookApi: BookApi = {
     ...book,
     price: String(book.price),
   };
-  const response = await fetch(`${base_url}/books/${book.id}`, {
-    method: 'PUT',
-    body: JSON.stringify(bookApi),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+  return new Promise((resolve, reject) => {
+    try {
+      const req = https.request(
+        `${base_url}/books/${book.id}`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+        (res) => {
+          if (res.statusCode !== 200) {
+            reject(new Error('Api error'));
+          }
+          let data = '';
+          res.on('data', (chunk) => {
+            data += chunk;
+          });
+          res.on('end', () => {
+            const book: BookApi = JSON.parse(data);
+            revalidateBooks();
+            revalidateBookById(book.id);
+            resolve({
+              ...book,
+              price: Number(book.price),
+            });
+          });
+        },
+      );
+      req.write(JSON.stringify(bookApi));
+      req.end();
+    } catch (error) {
+      reject(error);
+    }
   });
-  const data: BookApi = await response.json();
-  await revalidateBooks();
-  await revalidateBookById(book.id);
-  return {
-    ...data,
-    price: Number(data.price),
-  };
 };
 
 export const deleteBookById = async (id: string): Promise<void> => {
-  await fetch(`${base_url}/books/${id}`, {
-    method: 'DELETE',
+  return new Promise((resolve, reject) => {
+    try {
+      const req = https.request(
+        `${base_url}/books/${id}`,
+        {
+          method: 'DELETE',
+        },
+        (res) => {
+          if (res.statusCode !== 200) {
+            reject(new Error('Api error'));
+          }
+          revalidateBooks();
+          revalidateBookById(id);
+          resolve();
+        },
+      );
+      req.end();
+    } catch (error) {
+      reject(error);
+    }
   });
-  await revalidateBooks();
-  await revalidateBookById(id);
 };
 
 export const batchDeleteBooks = async (ids: string[]): Promise<void> => {
